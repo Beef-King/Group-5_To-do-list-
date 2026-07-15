b4d2a17ed3d267e717d207b08d609b70db3a2966
Beef-King <uwemedimookon124@gmail.com>
2026-07-15 07:19:57 +0100

    add flask routes for the 2 more html pages in the prooject

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/create")
+def create_task():
+    return render_template("create_task.html")
+
+@app.route("/tasks")
+def view_tasks():
+    return render_template("view_tasks.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
