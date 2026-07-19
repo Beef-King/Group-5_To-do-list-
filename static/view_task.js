33722d585987637f82b6851ceceffd0b63ae3dd4
Beef-King <uwemedimookon124@gmail.com>
2026-07-19 16:27:18 +0100

    frontend for the view task page is compkete, remains the logic

--- static/view_task.js
+++ static/view_task.js
@@ -1,6 +1,39 @@
+// =====================
+// Select Button
+// =====================
 const selectBtn = document.querySelector(".select-btn");
 const deleteBtn = document.querySelector(".delete-btn");
 
-selectBtn.addEventListener("click", () => {
-    deleteBtn.style.display = "inline-block";
-});
+if (selectBtn && deleteBtn) {
+    selectBtn.addEventListener("click", () => {
+        deleteBtn.style.display = "inline-block";
+    });
+}
+
+// =====================
+// Search
+// =====================
+const searchInput = document.getElementById("searchInput");
+
+if (searchInput) {
+    searchInput.addEventListener("input", function () {
+        console.log(this.value.trim());
+    });
+}
+
+// =====================
+// Status Tabs
+// =====================
+const tabs = document.querySelectorAll(".status-tab");
+
+if (tabs.length > 0) {
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+
+            tabs.forEach(btn => btn.classList.remove("active"));
+
+            tab.classList.add("active");
+
+        });
+    });
+}
